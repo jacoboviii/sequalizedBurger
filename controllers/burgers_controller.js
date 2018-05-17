b5eefd0a7a8e69ada018b21ccc40e0beecfbde1b
Jacobo Perez <jacoboperez@Jacobos-MacBook-Pro.local>
2018-05-16 23:17:11 -0600

    working app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,40 @@
-const express = require('express');
+const db = require("../models");
 
-const router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
-const burger = require('../models/burgers.js');
-
-// Create all our routes and set up logic within those routes where required.
-router.get('/', function(req, res) {
-  burger.selectAll(function(data) {
-    let hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+module.exports = function (app) {
+  
+  // GET route for getting all of the burgers
+  app.get("/", function (req, res) {
+    // Add sequelize code to find all burgers, and return them to the user with res.render
+    db.Burger.findAll({}).then(function (dbBurgers) {
+      let hbsObject = {
+        burgers: dbBurgers
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
   });
-});
 
-router.post('/api/burgers', function(req, res) {
-    console.log(req.body)
-    burger.insertOne('burger_name', [req.body.burger_name], function(result) {
-      // Send back the ID of the new quote
-      console.log(result)
+   // POST route for saving a new burger
+   app.post("/api/burgers", function (req, res) {
+    // Add sequelize code for creating a post using req.body,
+    // then return the result using res.json
+    db.Burger.create(req.body).then(function (dbBurger) {
+      console.log(dbBurger);
       res.end();
     });
   });
 
-  router.put("/api/burgers/:id", function(req, res) {
-    const condition = "id = " + req.params.id;
-  
-    burger.updateOne('devoured', 1, 'id', req.params.id, function(result) {
-
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
+  // PUT route for updating burgers
+  app.put("/api/burgers/:id", function (req, res) {
+    // Add code here to update a burger using the values in req.body, where the id is equal to
+    // req.params.id and return the result to the user using res.json
+    db.Burger.update(req.body,
+      {
+        where: {
+          id: req.params.id
         }
+      }).then(function () {
         res.status(200).end();
-  
-      }
-    );
+      });
   });
-// Export routes for server.js to use.
-module.exports = router;
+}
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Requiring our models for syncing
+const db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -21,11 +24,15 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
+// Routes
+// =============================================================
+require("./controllers/burgers_controller.js")(app);
 
-app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
